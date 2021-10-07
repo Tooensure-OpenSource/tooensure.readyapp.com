3b4d1e636946581d42c50cb66a48f293751e9a8d
Shawn Bellazan <tooensuremaster@gmail.com>
2021-10-06 20:28:48 -0500

    Business Object --> Creating representation at its bare min.

---
+++ ReadyApp.Domain/Business.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadyApp.Domain
+{
+    internal class Business
+    {
+        public int BusinessId { get; private set; }
+        public string Name {  get; private set; }
+        public string Description {  get; private set; }
+        public string Type {  get; private set; }
+
+        // Constructor instances
+        public Business() { }
+        /// <summary>
+        /// Create a instance of a business object
+        /// </summary>
+        /// <param name="name">Sets name of business</param>
+        /// <param name="description">Sets description of business</param>
+        public Business(string name, string description)
+        {
+            Name = name;
+            Description = description;
+        }
+    }
+}

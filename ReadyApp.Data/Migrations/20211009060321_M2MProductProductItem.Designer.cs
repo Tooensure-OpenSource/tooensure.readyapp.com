8e8369774dcebebd593976cc7f97e86fa4e72f94
Shawn Bellazan <tooensuremaster@gmail.com>
2021-10-09 01:06:14 -0500

    M2M  relationship wihth product and product item migration

--- ReadyApp.Data/Migrations/20211009060321_M2MProductProductItem.Designer.cs
+++ ReadyApp.Data/Migrations/20211009060321_M2MProductProductItem.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ReadyApp.Data;
 
 namespace ReadyApp.Data.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20211009060321_M2MProductProductItem")]
+    partial class M2MProductProductItem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ReadyApp.Data/Migrations/20211009060321_M2MProductProductItem.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ReadyApp.Data.Migrations
+{
+    public partial class M2MProductProductItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ProductItems_Products_ProductId",
+                table: "ProductItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ProductItems_ProductId",
+                table: "ProductItems");
+
+            migrationBuilder.DropColumn(
+                name: "ProductId",
+                table: "ProductItems");
+
+            migrationBuilder.CreateTable(
+                name: "ProductProductItem",
+                columns: table => new
+                {
+                    ProductItemsProductItemId = table.Column<int>(type: "int", nullable: false),
+                    ProductReferancesProductId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductProductItem", x => new { x.ProductItemsProductItemId, x.ProductReferancesProductId });
+                    table.ForeignKey(
+                        name: "FK_ProductProductItem_ProductItems_ProductItemsProductItemId",
+                        column: x => x.ProductItemsProductItemId,
+                        principalTable: "ProductItems",
+                        principalColumn: "ProductItemId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProductProductItem_Products_ProductReferancesProductId",
+                        column: x => x.ProductReferancesProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductProductItem_ProductReferancesProductId",
+                table: "ProductProductItem",
+                column: "ProductReferancesProductId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductProductItem");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProductId",
+                table: "ProductItems",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductItems_ProductId",
+                table: "ProductItems",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ProductItems_Products_ProductId",
+                table: "ProductItems",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "ProductId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}

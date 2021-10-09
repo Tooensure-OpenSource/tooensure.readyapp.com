b2646f86700709112405acb28773d800139ead79
Shawn Bellazan <tooensuremaster@gmail.com>
2021-10-09 01:24:25 -0500

    Order Order Item many to many fuctionailty

--- ReadyApp.Domain/Order.cs
+++ ReadyApp.Domain/Order.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace ReadyApp.Domain
 {
-    internal class Order
+    public class Order
     {
+        public int OrderId { get; private set; }
+        public DateTime dateTime { get; set; }
+        public bool isReady { get; set; }
+        public List<OrderItem>? OrderItems { get; set; }
+
+        /// <summary>
+        /// Create Order instance and also create a instance of order items
+        /// </summary>
+        public Order()
+        {
+            OrderItems = new List<OrderItem>();
+        }
     }
 }
---
+++ ReadyApp.Domain/OrderItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadyApp.Domain
+{
+    public class OrderItem
+    {
+        public int OrderItemId { get; private  set; }
+        public int Quantity { get; set; }
+        public List<Order>? OrderReferances { get; set; }
+        /// <summary>
+        /// When creating a instance of order a order item instance can create a new referance 
+        /// </summary>
+        public OrderItem()
+        {
+            OrderReferances = new List<Order>();
+        }
+    }
+}

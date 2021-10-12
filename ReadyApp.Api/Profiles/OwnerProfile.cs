5a5fbcf8fab973567d9e7009b3fa2018fc16fca4
Shawn Bellazan <tooensuremaster@gmail.com>
2021-10-12 08:17:34 -0500

    Defining profiles for auto mapper

---
+++ ReadyApp.Api/Profiles/OwnerProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using ReadyApp.Api.Models;
+using ReadyApp.Domain;
+
+namespace ReadyApp.Api.Profiles
+{
+    public class OwnerProfile : Profile
+    {
+        public OwnerProfile()
+        {
+            CreateMap<Owner, OwnerDto>()
+                .ForMember(
+                    dest => dest.OwnerId,
+                    opt => opt.MapFrom(src => src.OwnerId))
+                .ForMember(
+                    dest => dest.UserId,
+                    opt => opt.MapFrom(src => src.UserId))
+                .ForMember(
+                    dest => dest.BusinessId,
+                    opt => opt.MapFrom(src => src.BusinessId));
+        }
+    }
+}
---
+++ ReadyApp.Api/Profiles/UserProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using ReadyApp.Api.Models;
+using ReadyApp.Domain.Entity;
+
+namespace ReadyApp.Api.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User, UserDto>()
+                .ForMember(
+                    dest => dest.UserId,
+                    opt => opt.MapFrom(src => src.UserId))
+                .ForMember(
+                    dest => dest.Name,
+                    opt => opt.MapFrom(src => $"{src.LastName}, {src.FirstName}"))
+                .ForMember(
+                    dest => dest.EmailAddress,
+                    opt => opt.MapFrom(src => src.Email))
+                .ForMember(
+                        dest => dest.Username,
+                        opt => opt.MapFrom(src => src.Username));
+
+        }
+    }
+}

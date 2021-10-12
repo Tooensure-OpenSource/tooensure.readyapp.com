db4f1e0faf87470bde134e5f14224187d83595f4
Shawn Bellazan <tooensuremaster@gmail.com>
2021-10-12 08:16:52 -0500

    Repositories implementating I interfaces

---
+++ ReadyApp.Api/Repositories/BusinessesRepository.cs
@@ -0,0 +1,25 @@
+﻿using ReadyApp.Data;
+using ReadyApp.Data.Services;
+using ReadyApp.Domain.Entity;
+
+namespace ReadyApp.Api.Repositories
+{
+    public class BusinessesRepository : IBusinessRepository
+    {
+        private readonly DataContext _dataContext;
+
+        public BusinessesRepository(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        public Business GetBusiness(Guid id)
+        {
+            return _dataContext.Businesses.FirstOrDefault(b => b.BusinessId == id);
+        }
+
+        public IEnumerable<Business> GetBusinesses()
+        {
+            return _dataContext.Businesses.ToList();
+        }
+    }
+}

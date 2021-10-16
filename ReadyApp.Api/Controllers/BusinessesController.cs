9a1a032f02272c4b4fee8507e8416c78158593fc
Shawn Bellazan <tooensuremaster@gmail.com>
2021-10-15 21:31:16 -0500

    Business controller Get search functionality

--- ReadyApp.Api/Controllers/BusinessesController.cs
+++ ReadyApp.Api/Controllers/BusinessesController.cs
@@ -26,22 +26,9 @@ namespace ReadyApp.Api.Controllers
                 throw new ArgumentNullException(nameof(mapper));
         }
 
-        /// <summary>
-        /// Every user has access to view every business
-        /// </summary>
-        /// <returns></returns>
-        //[HttpGet]
-        //public ActionResult<IEnumerable<BusinessDto>> GetBusinesses()
-        //{
-        //    var businessesFromRepo = _businessRepository.GetBusinesses();
-        //    var result = _mapper.Map<IEnumerable<BusinessDto>>(businessesFromRepo);
-
-        //    return Ok(result);
-        //}
-
-        
+ 
         // All users can only view business data through username search
-        [HttpGet]
+        [HttpGet()]
         [HttpHead]
         public ActionResult<IEnumerable<BusinessDto>> GetBusinesses(
             [FromQuery] BusinessResorcesParameters businessResorcesParameters)
@@ -69,17 +56,20 @@ namespace ReadyApp.Api.Controllers
         [HttpPost]
         public ActionResult<BusinessDto> RegisterBusiness(BusinessRegisterDto businessRegister)
         {
+            // *** START LOGIC ***
             // Mapping new business register data into business object
             var business = _mapper.Map<Business>(businessRegister);
             // checking if business is in fact a object with content (because of BusinessRegister reqiured attributes, There should always be content in User object)
             if (business == null) return NoContent();
-
+            // cheacking if there already a business in data store with username. There must not be a business with same username
             if (_businessRepository.BusinessExistByUsername(business.Username)) return BadRequest();
-      
+            // assuming all steps was passed then register business into data store
             _businessRepository.RegisterBusiness(business);
-
+            // save updated data in data store
             _businessRepository.Save();
+            // *** END LOGIC ***
 
+            // mapping pre-exist data into business dto (could of returned the business. may return business object rether then dto in future implementation)
             var businessToReturn = _mapper.Map<BusinessDto>(business);
             return CreatedAtRoute(
                 "GetBusiness",
